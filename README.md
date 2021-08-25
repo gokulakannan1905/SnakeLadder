adc576d45043ca4fee0c8e22cc214263cbe23825
Gokulakannan <gokulakannan.a@hotmail.com>
2021-08-25 15:51:27 +0530

    finding player position in SnakeLadder game

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# HelloWorld
+I am Learning programming concepts.
+
+This repo contains snake and ladder game using c++
+
+This is my initial code of the game.
+RollDie method is used as  Die(dice) in the game.
+I have implemented the logic for finding the position of the player in the game.
+snakes and ladders are implemented using dictionary/map datatype.
---
+++ snakeladder.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<stdlib.h>
+#include<map>
+#include<time.h>
+#include<math.h>
+using namespace std;
+int rollDie(){
+    srand(time(0));
+    return 1+rand()%6;
+}
+void printRowCol(int boardsize,int position){
+    int x,y;
+    x=(position/boardsize)+1;
+        if(x%2){
+           y=position%boardsize;
+           if(position%boardsize==0)
+           {
+               x--;
+               y++;
+           }
+        }
+        else{
+           y=(boardsize+1)-(position%boardsize);
+           if(position%boardsize==0)
+           {
+               x--;
+               y--;
+           }
+        }
+        cout<<position<<" -->  ("<<x<<", "<<y<<")\n";
+}
+int main(){
+
+    int boardsize,position;
+    while(1){
+    cout<<"Enter the position of player :\n";
+    cin>>position;
+    if(position == 0)
+        break;
+    boardsize =10;
+//    int snakeCount,ladderCount;
+//    snakeCount = round(boardsize*boardsize*0.10);
+//    ladderCount = round(boardsize*boardsize*0.05);
+    map<int,int> snake,ladder;
+
+    //ladder starting point and ending point
+    ladder[1]  = 23;
+    ladder[8]  = 14;
+    ladder[27] = 63;
+    ladder[67] = 73;
+    ladder[80] = 98;
+
+    //smake starting point and ending point
+    snake[99] = 54;
+    snake[94] = 42;
+    snake[91] = 71;
+    snake[85] = 62;
+    snake[88] = 31;
+    snake[78] = 38;
+    snake[53] = 26;
+    snake[46] = 23;
+    snake[28] = 10;
+    snake[16] = 3;
+
+    ladder[position]!=0?position=ladder[position]:position;
+    snake[position]!=0?position=snake[position]:position;
+
+
+
+    printRowCol(boardsize,position);
+    }
+    return 0;
+}
+
+
